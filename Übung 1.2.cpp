ae5e133cc759a59ceddb8103d4761a7d9c7f6f01
Cuba Dunn <116181380+skrrtmane@users.noreply.github.com>
2022-10-27 08:22:01 +0200

    Add files via upload

---
+++ Übung 1.2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	cout << "  _      _      _\n";
+	cout << "> (.)__ < (.)__ = (.)__\n";
+	cout << "(___ / (___ / (___ / \n";
+	return 0;
+
+}
